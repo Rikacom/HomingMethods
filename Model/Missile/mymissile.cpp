dc58606806d0a7adafc47191f1244fa9e5628be8
Irina M <IIrinaMay@yandex.ru>
2022-12-24 19:51:46 +0300

    Завершена задача поиска угла, чтобы ракета попала в цель. Система координат x, y (учтены все варианты *кроме координаты уже совпали и модуль скорости ракеты = 0)

--- Model/Missile/mymissile.cpp
+++ Model/Missile/mymissile.cpp
@@ -63,57 +63,154 @@ void CMyMissile::initiate()
     //              *    fi
     //              M ракета (x02=x,y02=y,vmod2=v_mod)
 
+    double T;//время
 
     //v_mod1=√(vx1*vx1+vy1*vy1) - модуль скорости цели
     double v_mod1=sqrt((*targetData->vx_t)*(*targetData->vx_t)+(*targetData->vy_t)*(*targetData->vy_t));
-
-    //единичный вектор TaO* = {vx1,vy1}
-    //вектор TaM = {x02-x01,y02-y01}
-    //зная два вектора можем вычислить угол gamma
-    double gamma=acos(
-                ((x-*targetData->x_t)*(*targetData->vx_t)+(y-*targetData->y_t)*(*targetData->vy_t))
-                /
-                (sqrt((x-*targetData->x_t)*(x-*targetData->x_t)+(y-*targetData->y_t)*(y-*targetData->y_t))
-                 *sqrt((*targetData->vx_t)*(*targetData->vx_t)+(*targetData->vy_t)*(*targetData->vy_t)))
-                );
-
-    //из точки O проводим высоту h
-    //sin(gamma)=h/B     ->     h=B*sin(gamma)
-    //sin(beta)=h/C=B*sin(gamma)/C
-    //B=v_mod1*T ,где T-время
-    //C=v_mod2*T
-    //sin(beta)=v_mod1*sin(gamma)/v_mod2
-    //если нарисовать дугой треугольник, то может возникнуть ситуация sin(180-beta)=v_mod1*sin(gamma)/v_mod2
-    //но sin(beta)=sin(180-beta)=v_mod1*sin(gamma)/v_mod
-    //*были рассмотренеы все виды треуголников и sin(угла)=v_mod1*sin(gamma)/v_mod2
-    //
-    //получается два угла beta (beta1 и beta2):
-    //beta1=asin(v_mod1*sin(gamma)/v_mod);       - острый
-    //beta2=M_PI-asin(v_mod1*sin(gamma)/v_mod);  - тупой
-    //
-    //существование beta2 объясняется тем, что gamma - может быть острым углом
-    //но мы все равно должны проверить, что сумма 2ух углов меньше 180     ->     (gamma+beta2)<180
-    double beta1=asin(v_mod1*sin(gamma)/v_mod);
-    double beta2=M_PI-asin(v_mod1*sin(gamma)/v_mod);
-    double beta=beta1; //для всех вариантов подойдет острый угол
-    if((gamma+beta2)<(180*gd_RAD)) beta=beta2; //для случая gamma-острый и сумма (gamma+beta2)<180 есть два варианта решения
 
     //вычисляем расстояние A между точками Ta и M
     //A=sqrt((x01-x02)*(x01-x02)+(y01-y02)*(y01-y02))
     double A=sqrt((*targetData->x_t-x)*(*targetData->x_t-x)+(*targetData->y_t-y)*(*targetData->y_t-y));
 
-    //TaO=B=v_mod1*T=[формула нахождения стороны через два угла и сторону]=A*sin(beta)/sin(gamma+beta)
-    //выражаем T и вычисляем
-    double T=A*sin(beta)/(sin(gamma+beta)*v_mod1);
+    if(v_mod1==0) // если цель не летит
+        {
+        //A=v_mod2*T
+        T=A/v_mod;
 
-    //M=(x02,y02)
-    //O=(x01+vx1*T,y01+vy1*T)
-    //вектор MO перемещаем в начало координат (будет вектор M1O1)
-    //M=(0,0)
-    //O=(x01+vx1*T-x02,y01+vy1*T-y02)=(O_x,O_y)
-    //tan(fi)=O_y/O_x
-    //fi=atan(O_y/O_x)
-    fi=atan2((*targetData->y_t+(*targetData->vy_t)*T-y),(*targetData->x_t+(*targetData->vx_t)*T-x));
+        //M=(x02,y02)
+        //Ta=(x01,y01)
+        //вектор MTa перемещаем в начало координат (будет вектор M1Ta1)
+        //M=(0,0)
+        //Ta1=(x01-x02,y01-y02)=(Ta1_x,Ta1_y)
+        //tan(fi)=Ta1_y/Ta1_x
+        //fi=atan(Ta1_y/Ta1_x)
+        fi=atan2(*targetData->y_t-y,*targetData->x_t-x);
+        }
+    else
+        {
+        //единичный вектор TaO* = {vx1,vy1}
+        //вектор TaM = {x02-x01,y02-y01}
+        //зная два вектора можем вычислить угол gamma
+        double gamma=acos(
+                    ((x-*targetData->x_t)*(*targetData->vx_t)+(y-*targetData->y_t)*(*targetData->vy_t))
+                    /
+                    (sqrt((x-*targetData->x_t)*(x-*targetData->x_t)+(y-*targetData->y_t)*(y-*targetData->y_t))
+                     *sqrt((*targetData->vx_t)*(*targetData->vx_t)+(*targetData->vy_t)*(*targetData->vy_t)))
+                    );
+
+        if(gamma==(180*gd_RAD)) //цель будет отдаляться от ракеты
+            {
+            if(v_mod<v_mod1) //если v_modракеты<v_modцели, то ракета не долетит
+                {
+                gpc_Task->bo_FinishFlag = true;   //прекращаем текущую реализацию задачи
+                }
+            else
+                {
+
+    //          * O точка встречи
+    //            ---
+    //                -<-           C
+    //           B         * Ta цель (x01,yo1,vx1,vy1)
+    //                         ---
+    //                             -<-      fi
+    //                    A             *
+    //                                  M ракета (x02=x,y02=y,vmod2=v_mod)
+
+                //B=v_mod1*T
+                //C=v_mod2*T=B+A=v_mod1*T+A
+                //выражаем T и вычисляем
+                T=A/(v_mod-v_mod1);
+
+                //M=(x02,y02)
+                //Ta=(x01,y01)
+                //вектор MTa перемещаем в начало координат (будет вектор M1Ta1)
+                //M=(0,0)
+                //Ta1=(x01-x02,y01-y02)=(Ta1_x,Ta1_y)
+                //tan(fi)=Ta1_y/Ta1_x
+                //fi=atan(Ta1_y/Ta1_x)
+                fi=atan2(*targetData->y_t-y,*targetData->x_t-x);
+                }
+            }
+        else
+            {
+            if(!gamma) //цель будет лететь на встречу ракете
+                {
+
+    //          * Ta цель (x01,yo1,vx1,vy1)
+    //            ->-
+    //                ---  B
+    //                     *  O точка встречи
+    //               A         ---
+    //                             -<- C    fi
+    //                                  *
+    //                                  M ракета (x02=x,y02=y,vmod2=v_mod)
+
+                //B=v_mod1*T
+                //C=v_mod2*T=A-B=A-v_mod1*T
+                //выражаем T и вычисляем
+                T=A/(v_mod+v_mod1);
+
+                //M=(x02,y02)
+                //Ta=(x01,y01)
+                //вектор MTa перемещаем в начало координат (будет вектор M1Ta1)
+                //M=(0,0)
+                //Ta1=(x01-x02,y01-y02)=(Ta1_x,Ta1_y)
+                //tan(fi)=Ta1_y/Ta1_x
+                //fi=atan(Ta1_y/Ta1_x)
+                fi=atan2(*targetData->y_t-y,*targetData->x_t-x);
+                }
+            else // получается треугольник
+                {
+                //из точки O проводим высоту h
+                //sin(gamma)=h/B     ->     h=B*sin(gamma)
+                //sin(beta)=h/C=B*sin(gamma)/C
+                //B=v_mod1*T ,где T-время
+                //C=v_mod2*T
+                //sin(beta)=v_mod1*sin(gamma)/v_mod2
+                //если нарисовать дугой треугольник, то может возникнуть ситуация sin(180-beta)=v_mod1*sin(gamma)/v_mod2
+                //но sin(beta)=sin(180-beta)=v_mod1*sin(gamma)/v_mod
+                //*были рассмотренеы все виды треуголников и sin(угла)=v_mod1*sin(gamma)/v_mod2
+                //
+                //получается два угла beta (beta1 и beta2):
+                //beta1=asin(v_mod1*sin(gamma)/v_mod)        - острый
+                //beta2=M_PI-asin(v_mod1*sin(gamma)/v_mod)   - тупой
+                //
+                //существование beta2 объясняется тем, что gamma - может быть острым углом
+                //но мы все равно должны проверить, что сумма 2ух углов меньше 180     ->     (gamma+beta2)<180
+                //
+                //для всех вариантов подойдет острый угол beta=beta1=asin(v_mod1*sin(gamma)/v_mod);
+                //if((gamma+beta2)<(180*gd_RAD)) beta=beta2=M_PI-asin(v_mod1*sin(gamma)/v_mod); //для случая gamma-острый и сумма (gamma+beta2)<180 есть два варианта решения
+                //
+                //
+                //но пример -> если цель летит слишком бысто, а ракета слишком медленно -> угол бессмысленно выбирать, тк ракета не долетит
+                //этот момент можно отследить:
+                //beta=asin(v_mod1*sin(gamma)/v_mod)   ->   если |(v_mod1*sin(gamma)/v_mod)|<=1, то угол beta существует
+                //                                          иначе нет (ракета не долетит)
+                if(fabs(v_mod1*sin(gamma)/v_mod)<=1)
+                    {
+                        double beta=asin(v_mod1*sin(gamma)/v_mod);
+
+                        //TaO=B=v_mod1*T=[формула нахождения стороны через два угла и сторону]=A*sin(beta)/sin(gamma+beta)
+                        //выражаем T и вычисляем
+                        T=A*sin(beta)/(sin(gamma+beta)*v_mod1);
+
+                        //M=(x02,y02)
+                        //O=(x01+vx1*T,y01+vy1*T)
+                        //вектор MO перемещаем в начало координат (будет вектор M1O1)
+                        //M=(0,0)
+                        //O=(x01+vx1*T-x02,y01+vy1*T-y02)=(O_x,O_y)
+                        //tan(fi)=O_y/O_x
+                        //fi=atan(O_y/O_x)
+                        fi=atan2((*targetData->y_t+(*targetData->vy_t)*T-y),(*targetData->x_t+(*targetData->vx_t)*T-x));
+                    }
+                else
+                    {
+                    gpc_Task->bo_FinishFlag = true;   //прекращаем текущую реализацию задачи
+                    }
+                }
+            }
+        }
+
 //заканчиваем вычислять угол fi<<
 
     vx=v_mod*cos(fi);

1979d6fea7739c58677d28fb49554283c54b1a8a
Irina M <IIrinaMay@yandex.ru>
2023-03-04 23:04:40 +0300

    Обновление и поддержка консольной версии

---
+++ CHomingMethods.pro
@@ -0,0 +1,52 @@
+include(../Common/Common.pri)
+
+QT       += core gui
+QT += widgets
+QT += xml
+QT += printsupport
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = HomingMethods
+TEMPLATE = app
+
+INCLUDEPATH += EXAMPLES
+INCLUDEPATH += Model/Missile
+INCLUDEPATH += Model/Target
+INCLUDEPATH += Tasks
+INCLUDEPATH += Tasks/ADD_NEW_TASK
+
+DEFINES += QT_DEPRECATED_WARNINGS
+DEFINES += MODEL
+
+OBJECTS_DIR = ./tmp/obj
+MOC_DIR = ./tmp/moc
+UI_DIR = ./tmp/ui
+RCC_DIR = ./tmp/rcc
+DESTDIR = $$_PRO_FILE_PWD_
+
+# You can also make your code fail to compile if you use deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+
+SOURCES += \
+        main.cpp \
+    Tasks/CreateTask.cpp \
+    Model/Target/Target.cpp \
+    Tasks/Matrosova.cpp \
+    Model/Target/Mytarget.cpp \
+    Model/Missile/MyMissile.cpp
+
+HEADERS += \
+    Tasks/CreateTask.h \
+    Model/Target/Target.h \
+    Tasks/ADD_NEW_TASK/ADD_Target.hpp \
+    Tasks/ADD_NEW_TASK/Target_List.hpp \
+    Tasks/ADD_NEW_TASK/Task_Include.hpp \
+    Tasks/ADD_NEW_TASK/Task_List.hpp \
+    Tasks/ADD_NEW_TASK/Task_New.hpp \
+    Tasks/Matrosova.h \
+    Model/Target/MyTarget.h \
+    Model/Missile/MyMissile.h
--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
-#include "mainwindow.h"
-#include "about.h"
-#include "plotter.h"
-// #include <QTranslator>
+
+
 #include <QApplication>
-#include <QEvent>
-#include <QDebug>
-#include <QDesktopWidget>
-#include <exception>
 #include "CreateTask.h"
 #include "Target.h"
+//#include "executeControl.h"
+#include "Global.h"
+
+
+//---------------------------------------------------------------------------------------------------------
+
+#ifdef USEGUI
+
+#include "mainwindow.h"
 
 extern MainWindow * fm_window;
-// bool statistic = 0;
 
 int main(int argc, char *argv[])
 {
@@ -20,10 +22,12 @@ int main(int argc, char *argv[])
     QApplication qapp(argc, argv);
     setlocale(LC_NUMERIC,"C");
     AppPath = QApplication::applicationDirPath();
+
 //#ifndef __unix__
 //    AppPath.truncate(AppPath.lastIndexOf('/')+1);
 //#else
     AppPath += "/";
+    gc_AppPath = AppPath.toStdWString();
 //#endif
     //QMessageBox::warning(fm_window, QString("FilePath"), QString("File ") + AppPath);
     fm_window = new MainWindow((AppPath), CreateTask, CreateTarget, DestroyTarget, gac_Task_Array, gac_Target_Array);
@@ -32,6 +36,104 @@ int main(int argc, char *argv[])
         QApplication::desktop()->availableGeometry()));
     fm_window->show();
     fm_window->loadLast();
-    int rezult = qapp.exec();
-    return rezult;
+    int result = qapp.exec();
+    return result;
 }
+
+#else
+#define DEBUG_INFO
+int auto_run(QString fileName);     //запуск на счет
+int main(int argc, char *argv[])
+{
+
+    QApplication capp(argc, argv);
+    setlocale(LC_ALL,"ru_RU.UTF-8");
+    setlocale(LC_NUMERIC,"C");
+
+#ifdef DEBUG_INFO
+    cout<<"Please enter arguments separated by spaces"<<endl;
+
+    std::cout<<"argc = "<<argc<<"\n";
+
+    for (int i = 0; i<argc; i++)
+    {
+        std::cout<<"argv["<<i<<"] = "<<argv[i]<<"\n";
+    }
+#endif
+    if (argc < 2)
+    {
+        control.write_error(ERROR_COUNT_ARG);
+        return ERROR_COUNT_ARG;
+    }
+    QString fileName = argv[1];
+    //CurFile = fileName;
+
+    if(argc >= 3)
+    {
+        QString flag1 = argv[2];
+        int flag = flag1.toInt();
+
+        if(flag==1)
+            control.nominal = true;
+    }
+
+    QString AppPath = QApplication::applicationDirPath();
+    AppPath += "/";
+    gc_AppPath = AppPath.toStdWString();
+    AppPath.truncate(AppPath.lastIndexOf('/')+1);
+#ifdef Q_OS_WIN
+    AppPath.truncate(AppPath.lastIndexOf('/'));
+    AppPath.truncate(AppPath.lastIndexOf('/')+1);
+#endif
+    //AppPath += rq("../");
+
+
+    //  Запуск на счет
+
+    return auto_run(fileName);
+
+    return 0;
+}
+
+int auto_run(QString fileName)
+{
+    int result = 0;
+    control.setCreateTask(CreateTask);//Даем классу указатели на функции создания задачи и цели
+    control.setCreateTarget(CreateTarget);
+    //  Загрузка варианта из файла
+    if (control.LoadFromFile(fileName))
+    {
+        control.write_error(ERROR_READ_FILE);
+        return ERROR_READ_FILE;
+    }
+    control.ReadGrafSettingsFromFile(fileName);
+#ifdef DEBUG_INFO
+    cout<<"Calculating starts"<<endl;
+#endif
+    //  Cчет
+    if(control.nominal)
+    {
+        ChuLib::zero_urand = true; //сбросить счетчики случайных чисел;
+    }
+    result = control.on_actionRun_triggered();
+
+#ifdef DEBUG_INFO
+    cout<<"Calculating finished. Saving to xml-file"<<endl;
+#endif
+    //  Запись результата в xml-файл
+    if (!control.WriteXml(fileName, 0))
+    {
+        control.write_error(ERROR_SAVE_FILE);
+        return ERROR_SAVE_FILE;
+    }
+
+    if(!control.Closed)
+    {
+        control.CloseVariant();  //закрыть вариант
+#ifdef DEBUG_INFO
+        cout<<"Closing Variant"<<endl;
+#endif
+    }
+    return result;
+}
+#endif
